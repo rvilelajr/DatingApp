24b84c227d772162014fdeee323be9577101ba0f
Rogerio Vilela <rogerio.vilela@tatic.net>
2020-10-30 20:05:59 -0400

    Extended API

---
+++ API/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace API.Extensions
+{
+    public static class DateTimeExtensions
+    {
+        public static int CalculateAge(this DateTime dob)
+        {
+            var today = DateTime.Today;
+            var age = today.Year - dob.Year;
+            return dob.Date > today.AddYears(-age) ? --age : age;
+        }
+    }
+}
